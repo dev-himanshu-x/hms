b966e40ee0e74d0765b6a1a96ff4cd33c4e7cb5e
Himanshu <iam.himanshu.x@gmail.com>
2025-11-29 20:51:59 +0530

    Appointment completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = angular.module('myapp', ['ui.router']);
-var url = "https://10.21.97.46:8001/";
+var url = "https://10.21.99.62:8001/";
 app.factory('httpInterceptor', function ($q, $rootScope, $log) {
     var numLoadings = 0
     return {
@@ -38,6 +38,22 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
             url: '/signin',
             templateUrl: 'pages/signin.html'
         })
+        .state('/', {
+            url: '/',
+            templateUrl: 'pages/land.html'
+        })
+        .state('dummy', {
+            url: '/dummy',
+            templateUrl: 'dummy.html'
+        })
+    $httpProvider.interceptors.push(function () {
+        return {
+            'request': function (config) {
+                config.withCredentials = true
+                return config;
+            }
+        };
+    })
 });
 app.directive("loader", function ($rootScope) {
     return function ($scope, element, attrs) {
@@ -49,7 +65,7 @@ app.directive("loader", function ($rootScope) {
         })
     }
 })
-app.controller('signup', function ($scope, $http) {
+app.controller('signup', function ($scope, $http, $state) {
     $http.get(url + "auth/dropdowns/").then(function (response) {
         var data = response.data
         $scope.genders = data.genders
@@ -78,14 +94,18 @@ app.controller('signup', function ($scope, $http) {
         if (password == confirm) {
             var form = document.getElementById("form")
             var data = new FormData(form)
-            console.log(data)
             $http.post(url + "auth/register", data, { headers: { 'Content-Type': undefined }, params: { 'role': 'doctor' } }).then(function (response) {
-                $state.go('signin');
+                $state.go('/');
+                Swal.fire({
+                    title: "Success",
+                    icon: "success",
+                    text: response.data.msg
+                });
             }).catch(function (error) {
                 Swal.fire({
                     title: "Error",
                     icon: "error",
-                    text: error.msg
+                    text: error.data.error
                 })
             });
         }
@@ -98,7 +118,7 @@ app.controller('signup', function ($scope, $http) {
         }
     }
 })
-app.controller('signupctrl', function ($scope, $http) {
+app.controller('signupctrl', function ($scope, $http, $state) {
     $http.get(url + "auth/dropdowns/").then(function (response) {
         var data = response.data
         $scope.genders = data.genders
@@ -126,14 +146,18 @@ app.controller('signupctrl', function ($scope, $http) {
         if (password == confirm) {
             var form = document.getElementById("form")
             var data = new FormData(form)
-            console.log(data)
             $http.post(url + "auth/register", data, { headers: { 'Content-Type': undefined }, params: { 'role': 'patient' } }).then(function (response) {
-                $state.go('signin');
+                $state.go('/');
+                Swal.fire({
+                    title: "Success",
+                    icon: "success",
+                    text: response.data.msg
+                });
             }).catch(function (error) {
                 Swal.fire({
                     title: "Error",
                     icon: "error",
-                    text: error.msg
+                    text: error.data.error
                 })
             });
         }
@@ -144,5 +168,88 @@ app.controller('signupctrl', function ($scope, $http) {
                 text: "Password and Confirm Password does not match"
             })
         }
+    }
+})
+app.controller('signin', function ($scope, $http, $state) {
+    $scope.submit = function () {
+        var data = {
+            email: $scope.email,
+            password: $scope.password
+        }
+        $http.post(url + "auth/login/", data, { withCredentials: true }).then(function (response) {
+            $state.go('/');
+            Swal.fire({
+                title: "Success",
+                icon: "success",
+                text: response.data.msg
+            });
+        }).catch(function (error) {
+            Swal.fire({
+                title: "Error",
+                icon: "error",
+                text: error.data.error
+            })
+        });
+    }
+})
+app.controller('land', function ($scope, $http, $state) {
+    $scope.dobcheck = function () {
+        var dob = $scope.dob;
+        var today = new Date();
+        today.setHours(0, 0, 0, 0);
+        if (dob < today) {
+            Swal.fire({
+                title: "Error",
+                icon: "error",
+                text: "Invalid Date"
+            });
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    $http.get(url + "auth/dropdowns/").then(function (response) {
+        var data = response.data
+        $scope.specialization = data.specializations
+    })
+    $http.get(url + "patient/doctors/").then(function (response) {
+        var doc = response.data
+        $scope.doctors = doc
+    }).catch(function (error) {
+        Swal.fire({
+            title: "Error",
+            icon: "error",
+            text: error.data.error
+        })
+    })
+    $scope.submit = function () {
+        var doctor = $scope.doctor
+        var date = $scope.dob.toLocaleDateString("sv-sv")
+        var time = $scope.time.toLocaleTimeString()
+        var reason_to_visit = $scope.reason_to_visit
+        var detail = {
+            doctor: doctor,
+            date: date,
+            time: time,
+            reason_to_visit: reason_to_visit
+        }
+        $http.post(url + "patient/bookAppointment/", detail, { withCredentials: true }).then(function (response) {
+            console.log(response)
+            Swal.fire({
+                title: "Success",
+                icon: "success",
+                text: response.data.msg
+            });
+        }).catch(function (error) {
+            Swal.fire({
+                title: "Error",
+                icon: "error",
+                text: error.data.error
+            })
+        })
+    }
+    $scope.clear = function () {
+        document.getElementById("Appointment").reset()
     }
 })
